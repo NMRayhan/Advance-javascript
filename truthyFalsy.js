e697a97351cea67f34a5ab314d1f563a626f49a3
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-03 16:48:40 +0600

    undefined are always falsy Value

--- truthyFalsy.js
+++ truthyFalsy.js
@@ -1,13 +1,14 @@
-const number = 0;
+let number;
 if(number > 0){
     console.log("Value is True");
 }else{
     console.log("Value is False");
 }
 
-const pName = ""
+let pName;
 if(pName){
     console.log("Value is True");
 }else{
     console.log("Value is False");
 }
+
