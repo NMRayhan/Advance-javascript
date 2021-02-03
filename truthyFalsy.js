7f0e2ef89ea368cf3bdaba81471fa14b9db64d9e
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-03 16:59:45 +0600

    empty Object and Array True Value

--- truthyFalsy.js
+++ truthyFalsy.js
@@ -1,7 +1,8 @@
-let value = " ", value1 = -2, value2 = 'hello';
-if (value && value1 && value2) {
+let value = " ", value1 = -2, value2 = 'hello', value3 = [], value4 = {};
+if (value && value1 && value2 && value3 && value4) {
     console.log("Value is True");
 } else {
     console.log("Value is False");
 }
 
+//empty String Falsy bt empty Array and Empty Object are Truthy
