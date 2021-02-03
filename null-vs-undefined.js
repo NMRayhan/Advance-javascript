e2ee5d4bd3d3196830c8bd34b96a431b183f0e22
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-03 17:35:35 +0600

    undefined all

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,8 +1,10 @@
 let number = undefined;
 console.log(number);
 
+
 let number1;
 console.log(number1);
+
 
 function addNum(num1,num2){
     let sum = num1 + num2;
@@ -11,11 +13,20 @@ function addNum(num1,num2){
 let result = addNum(12,12);
 console.log(result);
 
+
 function twoNum(num1,num2){
     return num2;
 }
 let result2 = twoNum(2)
 console.log(result2);
+
+
+function twoNumber(num1,num2){
+    console.log(num2);
+}
+let result3 = twoNumber(32)
+console.log(result3);
+
 
 const person={
     pName : "Salman Khan",
@@ -23,6 +34,7 @@ const person={
     pAge : 35
 }
 console.log(person.address);
+
 
 const arrayNumber = [1,2,3,4]
 console.log(arrayNumber[10]);
