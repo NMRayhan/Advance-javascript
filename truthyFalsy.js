b7ea14bc644dbafdb1277bb2bf767764725e358e
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-03 16:55:30 +0600

    All Are Truthy Value

--- truthyFalsy.js
+++ truthyFalsy.js
@@ -1,5 +1,5 @@
-let value = "", value1 = NaN, value2 = null, value3 = 0, value4;
-if (value && value1 && value2 && value3 && value4) {
+let value = " ", value1 = -2, value2 = 'hello';
+if (value && value1 && value2) {
     console.log("Value is True");
 } else {
     console.log("Value is False");
