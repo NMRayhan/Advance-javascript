31fafd3eb845f4f0d2b5e591162f57bbf93e133e
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-03 16:45:44 +0600

    Truthy Value

---
+++ truthyFalsy.js
@@ -0,0 +1,13 @@
+const number = 12;
+if(number > 0){
+    console.log("Value is True");
+}else{
+    console.log("Value is False");
+}
+
+const pName = "Rayhan"
+if(pName){
+    console.log("Value is True");
+}else{
+    console.log("Value is False");
+}
