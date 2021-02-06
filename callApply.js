0cfee2154b7504cbbb764c9a4635cb83006be471
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-06 11:26:30 +0600

    bind() Function Use in Multiple Object

--- callApply.js
+++ callApply.js
@@ -5,12 +5,22 @@ const student = {
     totalCourse : 30,
     cgpa : 3.8,
     getCourse : function(course){
-        return this.totalCourse -= course
+        this.totalCourse = this.totalCourse - course
+        return this.totalCourse;
     },
     getFullName : function(){
         return this.name + this.lastName;
     }
 }
-console.log(student.getFullName());
-console.log(student.totalCourse);
-console.log(student.getCourse(2));
+// console.log(student.getFullName());
+// console.log(student.totalCourse);
+// console.log(student.getCourse(2));
+
+const myFriend = {
+    name : "Ragvi Ahmed",
+    totalCourse : 40,
+    cgpa : 3.9
+}
+
+const myFriendCourse = student.getCourse.bind(myFriend)
+console.log(myFriendCourse(4));
