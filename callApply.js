8415ac85f61418e0b8421ce677641a7abb1a0a81
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-06 11:38:25 +0600

    Understanding bind() Function

--- callApply.js
+++ callApply.js
@@ -1,26 +1,55 @@
 const student = {
-    id : 2361,
-    name : "Nur Mohammad",
-    lastName : "Rayhan",
-    totalCourse : 30,
-    cgpa : 3.8,
+    id : null,
+    name : null,
+    lastName : null,
+    totalCourse : null,
+    cgpa : null,
     getCourse : function(course){
         this.totalCourse = this.totalCourse - course
-        return this.totalCourse;
+        return `total Completed Course : ${this.totalCourse}`;
     },
     getFullName : function(){
-        return this.name + this.lastName;
+        return `Student Name : ${this.name} ${this.lastName}`;
+    },
+    getCgpa : function(){
+        return `CGPA : ${this.cgpa}`;
     }
 }
 // console.log(student.getFullName());
 // console.log(student.totalCourse);
 // console.log(student.getCourse(2));
+// console.log(student.getFullName());
 
-const myFriend = {
+const friendTamim = {
     name : "Ragvi Ahmed",
+    lastName : "Tamim",
     totalCourse : 40,
     cgpa : 3.9
 }
 
-const myFriendCourse = student.getCourse.bind(myFriend)
-console.log(myFriendCourse(4));
+const friendChandan = {
+    name : "Chandan",
+    lastName : "Ojha",
+    totalCourse : 50,
+    cgpa : 4.0
+}
+
+//for Tamim
+const tamimName = student.getFullName.bind(friendTamim);
+console.log(tamimName());
+
+const tamimCourse = student.getCourse.bind(friendTamim);
+console.log(tamimCourse(5));
+
+const tamimCgpa = student.getCgpa.bind(friendTamim);
+console.log(tamimCgpa());
+
+//Chadan
+const chandanName = student.getFullName.bind(friendChandan);
+console.log(chandanName());
+
+const chandanCourse = student.getCourse.bind(friendChandan);
+console.log(chandanCourse(10));
+
+const chandanCgpa = student.getCgpa.bind(friendChandan);
+console.log(chandanCgpa());
