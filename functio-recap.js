363596e7ee53085d2536ed2506d9f25eae7803a9
NMRayhan <nurmdrayhan97@gmail.com>
2022-01-29 22:49:15 +0600

    just string reference

--- functio-recap.js
+++ functio-recap.js
@@ -1,11 +1,25 @@
 const singara = 10;
 
-function singaraNiyeAy(taka,count){
+function singaraNiyeAy(taka, count) {
     console.log("Chaca Singara Den ");
-    var totalTaka = (taka / singara)*count;
-    
+    var totalTaka = (taka / singara) * count;
+
     return (taka - totalTaka);
 }
 
-var finalTaka = singaraNiyeAy(100,4);
+var finalTaka = singaraNiyeAy(100, 4);
 console.log(finalTaka);
+
+function addNumber(number1, number2) {
+    return "Result is " + number1 + number2;
+}
+
+var result = addNumber(5, 4);
+console.log(result);
+
+let x ='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, quasi?';
+let y = 'Lorem';
+ console.log(x.localeCompare(y));
+let b = x.match(y);
+console.log(b);
+console.log(b.length);
