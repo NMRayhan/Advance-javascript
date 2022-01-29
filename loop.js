ae4293fa8cdd10aea71e1ac5311161e9bfd32d03
NMRayhan <nurmdrayhan97@gmail.com>
2022-01-29 15:32:32 +0600

    loop recap

---
+++ loop.js
@@ -0,0 +1,10 @@
+var user = 1;
+while (user <= 10) {
+    console.log("User Number " + user + ".");
+    user = user + 1;
+}
+console.log("liver server on");
+var finalNumber = 10;
+for (let index = 1; index <= finalNumber; index++) {
+    console.log("Number " + index);
+}
