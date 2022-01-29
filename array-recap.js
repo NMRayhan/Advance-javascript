471862b8cfed1a76b1710b256b3d9239c9e00721
NMRayhan <nurmdrayhan97@gmail.com>
2022-01-29 15:15:55 +0600

    array recap

---
+++ array-recap.js
@@ -0,0 +1,6 @@
+var books = ['Let Us C','Shoytaner birudde lorai', 'Java; the complete references', 'discrete mathematics','Object Oriented Programming','new headway pre-intermediate']
+
+console.log(books.length);
+books.push('Punnoboti');
+console.log(books.length);
+console.log(books);
