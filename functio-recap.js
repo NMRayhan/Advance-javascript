90dafc71ca1692305e237d7acfd361f1015a4a54
NMRayhan <nurmdrayhan97@gmail.com>
2022-01-30 22:33:04 +0600

    pizza ordering system

--- functio-recap.js
+++ functio-recap.js
@@ -19,12 +19,40 @@ isLeapYear(2300);
 */
 
 
-function factorialFinder(number) {
-    var factorial = 1
-    for (let i = number; i >= 1; i--) {
-        factorial = factorial * i;
+// function factorialFinder(number) {
+//     var factorial = 1
+//     for (let i = number; i >= 1; i--) {
+//         factorial = factorial * i;
+//     }
+//     return factorial;
+// }
+
+// console.log(factorialFinder(6));
+
+
+// function factorialFind(fact) {
+//     var i = 1;
+//     let number = 1;
+//     while (i <= fact) {
+//         number = number * i;
+//         i++;
+//     }
+//     return number;
+// }
+
+// console.log(factorialFind(5));
+
+function pizzaPanda(foodName, quantity) {
+    if (foodName.toLowerCase() == "pizza") {
+        var price = 10;
+        let total = price * quantity;
+        var message = "Your Order, " + quantity + " " + foodName.toLowerCase() + " and total you pay $" + total;
+        return message;
+    } else {
+        return "You have choice wrong food, please select only pizza";
     }
-    return factorial;
+
 }
 
-console.log(factorialFinder(6));
+
+console.log(pizzaPanda("PIzza", 5));
