b4eb2b11b944bcf4f6f87a3bc9e9e922d05f0cf0
NMRayhan <nurmdrayhan97@gmail.com>
2022-01-31 01:05:15 +0600

    calculate grade

--- functio-recap.js
+++ functio-recap.js
@@ -1,16 +1,22 @@
-//degree to fahrenheit formula, degree * (9/5) + 32
-
-function degreeToFahrenheit(degree) {
-    const fixed = 9/5;
-    return Math.ceil((degree * fixed) + 32);
+function calculateGrade(mark) {
+    let result = " ";
+    if (mark >= 0 && mark <= 32) {
+        result = "F";
+    } else if (mark >= 33 && mark <= 39) {
+        result = "D";
+    } else if (mark >= 40 && mark <= 49) {
+        result = "C";
+    } else if (mark >= 50 && mark <= 59) {
+        result = "B";
+    } else if (mark >= 60 && mark <= 69) {
+        result = "A-"
+    } else if (mark >= 70 && mark <= 79) {
+        result = "A"
+    } else if (mark >= 80 && mark <= 100) {
+        result = "A+"
+    } else {
+        result = "Please input valid number not negative or more then 100";
+    }
+    return result
 }
-console.log(degreeToFahrenheit(30));
-
-
-
-// fahrenheit to degree formula = (fahrenheit - 32) * (5/9)
-function fahrenheitToDegree(fahrenheit) {
-    let result = Math.ceil((fahrenheit - 32) * (5 / 9));
-    return result;
-}
-console.log(fahrenheitToDegree(10));
+console.log(calculateGrade());
