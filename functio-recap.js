76e0581ba2a24f3855b157e562107f1bb83ccf27
NMRayhan <nurmdrayhan97@gmail.com>
2022-02-02 02:41:27 +0600

    lowest price return in Object

--- functio-recap.js
+++ functio-recap.js
@@ -1,10 +1,18 @@
-let greeting = "Hello How Are You?"
+let mobiles= [
+    {name:"Samsung", price: 29000, camera: "16MP", productFrom: "Taiwan", memory: "6GB"},
+    {name:"HTC", price: 39000, camera: "32MP", productFrom: "Taiwan", memory: "8GB"},
+    {name:"Nokia", price: 55000, camera: "64MP", productFrom: "China", memory: "4GB"},
+    {name:"lenovo", price: 45000, camera: "64MP", productFrom: "India", memory: "6GB"},
+    {name:"Redmi", price: 48000, camera: "32MP", productFrom: "China", memory: "4GB"},
+]
 
-function reverseString(text) {
-    let reverseText = '';
-    for (let character of text) {
-        reverseText = character + reverseText;
+function lowestMobile(phones) {
+    let lowestPhone = phones[0];
+    for(const phone of phones ){
+        if(lowestPhone.price > phone.price){
+            lowestPhone = phone
+        }
     }
-    return reverseText;
+    return lowestPhone;
 }
-console.log(reverseString(greeting));
+console.log(lowestMobile(mobiles));
