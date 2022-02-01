5932e4123dcc4eb8c1ed4ede0d36f385ba67b60e
NMRayhan <nurmdrayhan97@gmail.com>
2022-02-01 22:46:42 +0600

    Find largest Number in Array

--- functio-recap.js
+++ functio-recap.js
@@ -1,9 +1,14 @@
-function birthDate(dayOfWeek, day, month, year) {
-    var daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-    return daysOfWeek[dayOfWeek] + 'day,' + day + ' ' + months[month] + ' ' + year;
+function LargesElement(Numbers) {
+    let largest = Numbers[0];
+    for (let count = 0; count < Numbers.length; count++) {
+        const element = Numbers[count];
+        if (element > largest) {
+            largest = element;
+        }
+    }
+    return largest;
 }
 
-var date1 = new Date(2000, 0, 1);
-console.log("Foo was Born on: " + birthDate(date1.getDay(), date1.getDate(), date1.getMonth(), date1.getFullYear()));
+// console.log(LargesElement([-2,-4,-10,-65,-5]));
+let largestArray = [10, 22, 3, 41, 5, 21, 6, 32, 45, 42, 45, 65, 63, 43, 5]
+console.log(LargesElement(largestArray));
