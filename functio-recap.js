e6dee250ac37c385d19de2bd8806ec786b1d2e99
NMRayhan <nurmdrayhan97@gmail.com>
2022-02-04 14:56:29 +0600

    Character Check is Upper case or Lower case if upper case then return full String upper case otherwise lower case return

--- functio-recap.js
+++ functio-recap.js
@@ -1,18 +1,22 @@
-let mobiles= [
-    {name:"Samsung", price: 29000, camera: "16MP", productFrom: "Taiwan", memory: "6GB"},
-    {name:"HTC", price: 39000, camera: "32MP", productFrom: "Taiwan", memory: "8GB"},
-    {name:"Nokia", price: 55000, camera: "64MP", productFrom: "China", memory: "4GB"},
-    {name:"lenovo", price: 45000, camera: "64MP", productFrom: "India", memory: "6GB"},
-    {name:"Redmi", price: 48000, camera: "32MP", productFrom: "China", memory: "4GB"},
-]
+let str = "MoHaMMad";
 
-function lowestMobile(phones) {
-    let lowestPhone = phones[0];
-    for(const phone of phones ){
-        if(lowestPhone.price > phone.price){
-            lowestPhone = phone
+function stringChanger(text) {
+    let upperCase = 0;
+    let lowerCase = 0;
+    for (const letter of text) {
+        if (letter == letter.toUpperCase()) {
+            upperCase = upperCase + 1;
+        } else {
+            lowerCase = lowerCase + 1;
         }
     }
-    return lowestPhone;
+    if (upperCase > lowerCase) {
+        return text.toUpperCase();
+    } else if(upperCase < lowerCase) {
+        return text.toLowerCase();
+    } else {
+        return text;
+    }
 }
-console.log(lowestMobile(mobiles));
+
+console.log(stringChanger(str));
